6d3de3a1f8397f8eda448defdf36e11d7dde25cc
Arina Mayrabeeva <mairabeeva42@gmail.com>
2026-02-17 15:17:56 +0300

    feat: add message handler with unknown command support

---
+++ bot/src/main/java/backend/academy/linktracker/bot/service/UpdateHandler.java
@@ -0,0 +1,31 @@
+package backend.academy.linktracker.bot.service;
+
+import backend.academy.linktracker.bot.commands.Command;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateHandler {
+    private final List<Command> commands;
+
+    public UpdateHandler(List<Command> commands) {
+        this.commands = commands;
+    }
+
+    public SendMessage process(Update update) {
+        if (update.message() == null || update.message().text() == null) {
+            return null;
+        }
+
+        Long chatId = update.message().chat().id();
+
+        for (Command command : commands) {
+            if (command.supports(update)) {
+                return command.handle(update);
+            }
+        }
+        return new SendMessage(chatId, "Неизвестная команда. Воспользуйтесь /help, чтобы посмотреть список доступных команд");
+    }
+}
