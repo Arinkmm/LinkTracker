a7a2146a2a4a31124d869713826e748a3aa62830
Arina Mayrabeeva <mairabeeva42@gmail.com>
2026-02-17 17:20:18 +0300

    feat: integrate UserRepository into StartCommand for user registration

--- bot/src/main/java/backend/academy/linktracker/bot/commands/StartCommand.java
+++ bot/src/main/java/backend/academy/linktracker/bot/commands/StartCommand.java
@@ -1,11 +1,19 @@
 package backend.academy.linktracker.bot.commands;
 
+import backend.academy.linktracker.bot.model.User;
+import backend.academy.linktracker.bot.repository.UserRepository;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartCommand implements Command {
+    private final UserRepository userRepository;
+
+    public StartCommand(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public String command() {
         return "/start";
@@ -19,6 +27,9 @@ public class StartCommand implements Command {
     @Override
     public SendMessage handle(Update update) {
         Long chatId = update.message().chat().id();
-        return new SendMessage(chatId,"Добро пожаловать! Используйте /help, чтобы посмотреть доступные команды");
+        if (!userRepository.exists(chatId)) {
+            userRepository.save(new User(chatId));
+        }
+        return new SendMessage(chatId, "Добро пожаловать! Используйте /help, чтобы посмотреть доступные команды");
     }
 }
