d1f4c8abd3bd268bfb667d7083eb6023e4758bf6
Arina Mayrabeeva <mairabeeva42@gmail.com>
2026-03-07 13:05:30 +0300

    feat: add unversioned files

---
+++ bot/src/main/java/backend/academy/linktracker/bot/client/impl/ScrapperGrpcClient.java
@@ -0,0 +1,93 @@
+package backend.academy.linktracker.bot.client.impl;
+
+import backend.academy.linktracker.bot.client.ScrapperClient;
+import backend.academy.linktracker.bot.exception.ApiException;
+import backend.academy.linktracker.bot.service.mapper.GrpcMapper;
+import backend.academy.linktracker.grpc.*;
+import backend.academy.linktracker.bot.dto.ApiErrorResponse;
+import backend.academy.linktracker.bot.dto.LinkResponse;
+import backend.academy.linktracker.bot.dto.ListLinksResponse;
+import io.grpc.StatusRuntimeException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.client.type", havingValue = "grpc")
+public class ScrapperGrpcClient implements ScrapperClient {
+    private final ScrapperServiceGrpc.ScrapperServiceBlockingStub blockingStub;
+    private final GrpcMapper mapper;
+
+    @Override
+    public void registerChat(Long id) {
+        try {
+            blockingStub.registerChat(
+                RegisterChatRequest.newBuilder().setId(id).build());
+        } catch (StatusRuntimeException e) {
+            throw new ApiException(toApiError(e));
+        }
+    }
+
+    @Override
+    public void deleteChat(Long id) {
+        try {
+            blockingStub.deleteChat(
+                DeleteChatRequest.newBuilder().setId(id).build());
+        } catch (StatusRuntimeException e) {
+            throw new ApiException(toApiError(e));
+        }
+    }
+
+    @Override
+    public LinkResponse addLink(Long id, String url, List<String> tags, List<String> filters) {
+        try {
+            backend.academy.linktracker.grpc.LinkResponse r =
+                blockingStub.addLink(AddLinkRequest.newBuilder()
+                    .setTgChatId(id)
+                    .setLink(url)
+                    .addAllTags(tags)
+                    .addAllFilters(filters)
+                    .build());
+            return mapper.fromProto(r);
+        } catch (StatusRuntimeException e) {
+            throw new ApiException(toApiError(e));
+        }
+    }
+
+    @Override
+    public LinkResponse removeLink(Long id, String url) {
+        try {
+            backend.academy.linktracker.grpc.LinkResponse r =
+                blockingStub.removeLink(RemoveLinkRequest.newBuilder()
+                    .setTgChatId(id).setLink(url).build());
+            return mapper.fromProto(r);
+        } catch (StatusRuntimeException e) {
+            throw new ApiException(toApiError(e));
+        }
+    }
+
+    @Override
+    public ListLinksResponse getLinks(Long id) {
+        try {
+            backend.academy.linktracker.grpc.ListLinksResponse r =
+                blockingStub.getLinks(GetLinksRequest.newBuilder()
+                    .setTgChatId(id).build());
+            return mapper.fromProto(r);
+        } catch (StatusRuntimeException e) {
+            throw new ApiException(toApiError(e));
+        }
+    }
+
+    private ApiErrorResponse toApiError(StatusRuntimeException e) {
+        String description = e.getStatus().getDescription();
+        return new ApiErrorResponse(
+            description,
+            String.valueOf(e.getStatus().getCode().value()),
+            e.getClass().getSimpleName(),
+            e.getMessage(),
+            List.of()
+        );
+    }
+}
---
+++ bot/src/main/java/backend/academy/linktracker/bot/commands/impl/ListCommand.java
@@ -0,0 +1,37 @@
+package backend.academy.linktracker.bot.commands.impl;
+
+import backend.academy.linktracker.bot.service.command.CommandExecutor;
+import backend.academy.linktracker.bot.commands.Command;
+import backend.academy.linktracker.bot.model.InternalCommand;
+import backend.academy.linktracker.bot.properties.CommandProperties;
+import com.pengrad.telegrambot.model.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class ListCommand implements Command {
+    private final CommandProperties commandProperties;
+    private final CommandExecutor commandExecutor;
+
+    @Override
+    public String command() {
+        return commandProperties.getCommands().get(InternalCommand.LIST.configKey).getName();
+    }
+
+    @Override
+    public String description() {
+        return commandProperties.getCommands().get(InternalCommand.LIST.configKey).getDescription();
+    }
+
+    @Override
+    public void handle(Message message) {
+        Long id = message.chat().id();
+        String text = message.text();
+
+        String[] parts = text.split(" ", 2);
+        String tag = parts.length > 1 ? parts[1] : null;
+
+        commandExecutor.executeList(id, tag);
+    }
+}
