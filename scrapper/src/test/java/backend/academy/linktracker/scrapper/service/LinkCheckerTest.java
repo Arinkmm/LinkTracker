1715d5577e7fc4fd381c60a9508ed76cac8f3241
Arina Mayrabeeva <mairabeeva42@gmail.com>
2026-03-29 01:28:33 +0300

    test: fix cases for link checker tests

--- scrapper/src/test/java/backend/academy/linktracker/scrapper/service/LinkCheckerTest.java
+++ scrapper/src/test/java/backend/academy/linktracker/scrapper/service/LinkCheckerTest.java
@@ -1,10 +1,12 @@
 package backend.academy.linktracker.scrapper.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 import backend.academy.linktracker.scrapper.dto.Link;
+import backend.academy.linktracker.scrapper.dto.Subscription;
 import backend.academy.linktracker.scrapper.properties.DBProperties;
 import backend.academy.linktracker.scrapper.properties.SchedulerProperties;
 import backend.academy.linktracker.scrapper.repository.LinkRepository;
@@ -52,8 +54,14 @@ class LinkCheckerTest {
 
     private LinkChecker linkChecker;
 
+    private final int PAGE_SIZE = 10;
+    private final int INTERVAL = 60000;
+
     @BeforeEach
     void setUp() {
+        when(dbProperties.getDefaultPageSize()).thenReturn(PAGE_SIZE);
+        when(schedulerProperties.getInterval()).thenReturn(INTERVAL);
+
         linkChecker = new LinkChecker(
                 linkRepository,
                 subscriptionRepository,
@@ -74,18 +82,25 @@ class LinkCheckerTest {
         Long tgChatId = 12345L;
 
         Link link = new Link(linkId, url, oldTime);
+        Subscription subscription = new Subscription(tgChatId, linkId, List.of("java"));
 
-        when(linkRepository.findStaleLinks(any(Instant.class), eq(0), eq(100))).thenReturn(List.of(link));
-        when(linkRepository.findStaleLinks(any(Instant.class), eq(1), eq(100))).thenReturn(List.of());
+        when(linkRepository.findStaleLinks(any(Instant.class), anyInt(), eq(PAGE_SIZE)))
+                .thenReturn(List.of(link))
+                .thenReturn(List.of());
 
         when(provider.supports(url)).thenReturn(true);
         when(provider.getCurrentTime(url)).thenReturn(Optional.of(newTime));
-        when(subscriptionRepository.findChatIdByLinkId(linkId)).thenReturn(List.of(tgChatId));
+
+        when(subscriptionRepository.findSubscriptionByChatId(eq(linkId), anyInt(), eq(PAGE_SIZE)))
+                .thenReturn(List.of(subscription))
+                .thenReturn(List.of());
+
         when(notificationBuilder.buildMessage(url)).thenReturn("Link updated!");
 
         linkChecker.checkAllLinks();
 
-        verify(linkRepository).updateLastChecked(linkId, newTime);
-        verify(botNotifier).notify(eq(linkId), eq(url), eq("Link updated!"), any());
+        verify(linkRepository, atLeastOnce()).updateLastChecked(eq(linkId), eq(newTime));
+
+        verify(botNotifier, atLeastOnce()).notify(eq(linkId), eq(url), eq("Link updated!"), eq(List.of(tgChatId)));
     }
 }
