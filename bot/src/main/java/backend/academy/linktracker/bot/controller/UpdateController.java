544daddefd0c66beb397f2773eda6492daded3c7
Arina Mayrabeeva <mairabeeva42@gmail.com>
2026-05-29 22:03:31 +0400

    fix: delete old classes

--- bot/src/main/java/backend/academy/linktracker/bot/controller/UpdateController.java
+++ bot/src/main/java/backend/academy/linktracker/bot/controller/UpdateController.java
@@ -2,7 +2,6 @@ package backend.academy.linktracker.bot.controller;
 
 import backend.academy.linktracker.bot.api.UpdatesApi;
 import backend.academy.linktracker.bot.dto.LinkUpdate;
-import backend.academy.linktracker.bot.metrics.BotMetrics;
 import backend.academy.linktracker.bot.service.bot.TelegramSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,20 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UpdateController implements UpdatesApi {
     private final TelegramSender telegramSender;
-    private final BotMetrics metrics;
 
     @Override
     public ResponseEntity<Void> updatesPost(LinkUpdate update) {
-        metrics.incrementTelegramRequest("http_update");
         log.atInfo()
                 .addKeyValue("chat_count", update.getTgChatIds().size())
                 .addKeyValue("description", update.getDescription())
                 .log("Sending link update");
 
-        update.getTgChatIds().forEach(id -> {
-            int sentMessages = telegramSender.sendMessage(id, update.getDescription());
-            metrics.incrementSentNotification(sentMessages);
-        });
+        update.getTgChatIds().forEach(id -> telegramSender.sendMessage(id, update.getDescription()));
 
         return ResponseEntity.ok().build();
     }
